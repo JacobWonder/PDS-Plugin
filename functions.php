2864526a5e0e209041850aa1f76f73c0930922c0
JacobWonder <59372346+JacobWonder@users.noreply.github.com>
2021-03-23 17:28:27 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -33,19 +33,30 @@ add_action( 'wp_enqueue_scripts', 'ds_ct_loadjs' );
 add_filter ( 'woocommerce_account_menu_items', 'misha_remove_my_account_links' );
 function misha_remove_my_account_links( $menu_links ){
  
-	unset( $menu_links['edit-address'] ); // Addresses
- 
- 
+	//unset( $menu_links['edit-address'] ); // Addresses
 	//unset( $menu_links['dashboard'] ); // Remove Dashboard
-	//unset( $menu_links['payment-methods'] ); // Remove Payment Methods
+	unset( $menu_links['payment-methods'] ); // Remove Payment Methods
 	//unset( $menu_links['orders'] ); // Remove Orders
 	//unset( $menu_links['downloads'] ); // Disable Downloads
-	//unset( $menu_links['edit-account'] ); // Remove Account details tab
+	unset( $menu_links['edit-account'] ); // Remove Account details tab
 	//unset( $menu_links['customer-logout'] ); // Remove Logout link
  
 	return $menu_links;
  
 }
+
+function my_account_menu_order() {
+ 	$menuOrder = array(
+ 		'orders'             => __( 'Your Payments', 'woocommerce' ),
+ 		'edit-address'       => __( 'Addresses', 'woocommerce' ),
+ 		'edit-account'    	=> __( 'Account Details', 'woocommerce' ),
+ 		'customer-logout'    => __( 'Log out', 'woocommerce' ),
+		'dashboard'          => __( 'Dashboard', 'woocommerce' )
+ 	);
+ 	return $menuOrder;
+ }
+ add_filter ( 'woocommerce_account_menu_items', 'my_account_menu_order' );
+
 
 include('login-editor.php');
 
